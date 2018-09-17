42d881ca63873b12bb8e938f6a80901f801e4ff4
mehulpanchal <mehulpanchal@users.noreply.github.com>
2018-09-17 16:37:48 +0530

    config calling changed based on laravel 5.5

--- src/Talk.php
+++ src/Talk.php
@@ -496,7 +496,7 @@ class Talk
             $receiver = $conversation->user_one;
         }
 
-        $userModel = $this->config('talk.user.model');
+        $userModel = config('talk.user.model');
         $user = new $userModel();
 
         return $user->find($receiver);
